7c6487e41ea5d5458f527497dfadaa42b9b97150
lucky-sugar-park <lucky.sugar.park@gmail.com>
2025-04-21 17:29:22 +0900

    사용자 관리모듈 분리

--- src/main/java/com/inzi/mes/be/common/user/impl/UserHistoryRepository.java
+++ src/main/java/com/inzi/mes/be/common/user/impl/UserHistoryRepository.java
@@ -1,10 +1,10 @@
-package com.inzi.mes.be.common.user.impl;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserHistoryRepository extends JpaRepository<UserHistoryEntity, String>, JpaSpecificationExecutor<UserHistoryEntity>{
-
-}
+//package com.inzi.mes.be.common.user.impl;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface UserHistoryRepository extends JpaRepository<UserHistoryEntity, String>, JpaSpecificationExecutor<UserHistoryEntity>{
+//
+//}
